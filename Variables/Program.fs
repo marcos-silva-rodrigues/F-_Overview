f6e136eb9374a5a520fa620a8ecba18294afe2e2
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-28 23:23:21 -0300

    feat: variaveis

---
+++ Variables/Program.fs
@@ -0,0 +1,14 @@
+﻿[<EntryPoint>]
+let main argv =
+  printfn "Welcome to the calculator program"
+
+  printfn "Type the first number"
+  let firstNo = System.Console.ReadLine()
+  printfn "Type the second number"
+  let secondNo = System.Console.ReadLine()
+  printfn "First %s, Second %s" firstNo secondNo
+
+  let sum = (int firstNo) + (int secondNo)
+  printfn "the sum is %i" sum
+  0
+  
