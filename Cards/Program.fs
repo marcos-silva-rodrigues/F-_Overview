0b313b3ebb0441f7bed15bb1c3485e9a4a702b41
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-29 00:05:03 -0300

    feat: funcoes

--- Cards/Program.fs
+++ Cards/Program.fs
@@ -1,9 +1,27 @@
-﻿let cardNo = 12
+﻿let cards = [21; 3; 1; 7; 9; 23]
+let cardFace card = 
+    let no = card % 13
+    if no = 1 then "Ace"
+    elif no = 0 then "King"
+    elif no = 12 then "Queen"
+    elif no = 11 then "Jack"
+    else string no
 
-let cardDescrition = 
-  if cardNo = 1 || cardNo = 14 then "Ace"
-  elif cardNo = 11 then "Jack"
-  elif cardNo = 12 then "Queen"
-  elif cardNo = 13 then "King"
-  else string cardNo
-printfn "%s" cardDescrition
+let suit card =
+    let no = card / 13
+    if no = 0 then "Hearts"
+    elif no = 1 then "Spades"
+    elif no = 2 then "Diamonds"
+    else "Clubs"
+
+let shuffle list =
+    let random = System.Random()
+    list |> List.sortBy (fun x -> random.Next())
+
+let printCard card = printfn "%s of %s" (cardFace card) (suit card)
+
+let printAll list = List.iter(fun x -> printCard(x)) list
+
+let take (no:int) (list) = List.take no list
+
+cards |> shuffle |> take 3 |> printAll
---
+++ Functions/Program.fs
@@ -0,0 +1,8 @@
+﻿let cardFace (card:int) :string = 
+  let no = card % 13
+  if no = 1 then "Ace"
+  elif no = 0 then "King"
+  elif no = 12 then "Queen"
+  elif no = 11 then "Jack"
+  else string no
+printfn "%s" (cardFace 11)
